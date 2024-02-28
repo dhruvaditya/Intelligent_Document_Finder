43e81d36394f54aded3774dbbc0ebc9c688491f4
Aditya Raj <cse.20201058@gmail.com>
2024-02-28 16:20:14 +0530

    Data retrieval from Google drive in real time and generating response

--- data.py
+++ data.py
@@ -1,67 +1,42 @@
+#Ok Tested Loader working Google drive Reader
+
+
 import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from googleapiclient.http import MediaIoBaseDownload
-import io
+from llama_index.readers.google import GoogleDriveReader
+import openai
+from dotenv import load_dotenv
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from llama_index.core import VectorStoreIndex
+from dotenv import load_dotenv
+# Load the .env file
+load_dotenv()
 
-# If modifying these SCOPES, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
-def print_file_content(service, file_id, file_name):
-    request = service.files().get_media(fileId=file_id)
-    file_io = io.BytesIO()
-    downloader = MediaIoBaseDownload(file_io, request)
-    done = False
-    while not done:
-        _, done = downloader.next_chunk()
-    file_io.seek(0)
-    content = file_io.read().decode('utf-8')
-    print(f"Content of {file_name} ({file_id}):\n{content}\n")
+# Access the API key
+openai_api_key = os.environ.get("OPENAI_API_KEY")
 
-def service_account_login():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    return build('drive', 'v3', credentials=creds)
-
-# Call the Drive v3 API
-def list_files(service, folder_id):
-    results = service.files().list(
-        q=f"'{folder_id}' in parents",
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-            print_file_content(service, item['id'], item['name'])
-
-#adding the folder id here
-service = service_account_login()
-list_files(service, '1RFhr3-KmOZCR5rtp4dlOMNl3LKe1kOA5')
+# documents = SimpleDirectoryReader("data").load_data()
 
 
+gauth=GoogleAuth()
+drive=GoogleDrive(gauth)
+
+loader = GoogleDriveReader()
+def load_data(folder_id: str):
+    docs = loader.load_data(folder_id=folder_id)
+    index = VectorStoreIndex.from_documents(docs)
+    query_engine = index.as_query_engine()
+    response = query_engine.query("summarize the file")
+    print(response)
+    
+    for doc in docs:
+        doc.id_ = doc.metadata["file_name"]
+    return docs
 
 
-
-
-
-
+docs = load_data(folder_id="1xCRk4ZdPH_OOp2fDulleilxKJEV3_ahD")
+# print(docs)
