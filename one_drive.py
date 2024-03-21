534e7f2b9425f27ab830a89c7f07f011feadb0de
Aditya Raj <cse.20201058@gmail.com>
2024-03-21 13:30:47 +0530

    modified one_drive retrieval

--- one_drive.py
+++ one_drive.py
@@ -2,15 +2,22 @@ import msal
 from llama_index.core import VectorStoreIndex, ServiceContext, Document
 from msal import PublicClientApplication
 from dotenv import load_dotenv
-# APPLICATION_ID='5bb08096-5db5-44ad-9fc2-48cc773ca582'
-# CLIENT_SECRET='YKV8Q~zkNg-9nuW91~cJeY.lpun_bRo0eSni3bTl'
+import openai
+from llama_index.core import Document, VectorStoreIndex
+from llama_index.core.llama_pack import download_llama_pack
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms.openai import OpenAI
+import os
+
 
 from llama_index.readers.microsoft_onedrive import OneDriveReader
+# Load the .env file
 load_dotenv()
 openai.api_key = os.environ["OPENAI_KEY"]
-APPLICATION_ID='d33f76d2-e4bc-432f-87db-88ffc1e40fc2'
 # User Authentication flow: Replace client id with your own id
-loader = OneDriveReader(client_id="d33f76d2-e4bc-432f-87db-88ffc1e40fc2")
+client_id = os.getenv('CLIENT_ID')
+loader = OneDriveReader(client_id)
 
 # APP Authentication flow: NOT SUPPORTED By Microsoft
 
